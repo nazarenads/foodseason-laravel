c844c5287d2537c780391f2d1b847d62169bad0b
nazarenads <nazarenads@gmail.com>
2019-07-26 22:40:35 -0300

    adapto register a laravel

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,83 @@
-@extends('layouts.app')
+@extends("layouts.template")
+@section("title")
+Foodseason - Registrarse
+@endsection
+@section("body")
+    @include('\partials\header')
+  <body class ="signin-signup-body">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <section class="register" id="register">
+          <div class="container-signin-signup">
+           <div class="form-signin-signup">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+          <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+                 @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    		   <h3 class="form-signup-heading">¡Registrate!</h3>
+    		   <div class="form-group">
+    		    <input name="email" type="text" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" placeholder="Email" >
+              @error('email')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+    		   </div>
+    		   <div class="form-group">
+    		    <input name="username" type="text" class="form-control @error('username') is-invalid @enderror" placeholder="Usuario" value="{{ old('name') }}">
+              @error('username')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+           </div>
+            {{-- <div class="form-group">
+    		    <input name="profilePicture" type="file" value="">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+    		   </div> --}}
+    		   <div class="form-group">
+    		    <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Contraseña" value="">
+              @error('password')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+           </div>
+            <div class="form-group">
+    		    <input type="password" class="form-control" name="passwordConfirm" placeholder="Confirmar contraseña" value= "">
+    		   </div>
+           <div class="form-group">
+             <select class="form-control" name="question" >
+               <option selected>Selecciona una pregunta de seguridad</option>
+               <option value="mascota">Nombre de tu primer mascota. </option>
+               <option value="comida">Nombre de tu comida favorita.</option>
+               <option value="infancia">Nombre de tu mejor amigo de la infancia.</option>
+             </select>
+           </div>
+           <div class="form-group">
+            <input type="password" class="form-control" name="secretAnswer" placeholder="Ingresa tu respuesta secreta" value= "">
+           </div>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+           <div class="form-group">
+          <input type="password" class="form-control" name="secretAnswerConfirm" placeholder="Confirma tu respuesta secreta" value= "">
+         </div>
+           <button type="submit" class="btn btn-warning btn-signin-signup" style="width:100%">
+               {{ __('Crear cuenta') }}
+           </button>
+    		   <br/><br>
+    		   <a class="btn btn-light btn-light-signin-signup" href="signin.php" role="button">¿Ya tenés una cuenta?</a>
+    		   <a class="btn btn-light btn-light-signin-signup" href="forgottenPass.php" role="button">¿Te olvidaste tu contraseña?</a>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+          </form>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+           </div>
+         </div>
+  </section>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
 @endsection
