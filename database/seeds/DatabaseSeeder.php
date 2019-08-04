a55d07a1047cc377ee257735155b9a87a731a8b6
lonmp <lonegromp@gmail.com>
2019-08-04 01:13:25 -0300

    dbSeeder acualizado para que vea seeds de tags y recipes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
          $this->call(UsersTableSeeder::class);
-
+         $this->call(TagsSeeder::class);
+         $this->call(RecipesTableSeeder::class);
+         
         Model::reguard();
     }
 }
