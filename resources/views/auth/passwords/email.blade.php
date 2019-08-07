7f699f69c1f6b6748dac84da7c5e124f3ab14ede
nazarenads <nazarenads@gmail.com>
2019-08-07 01:21:28 -0300

    recuperar contraseña

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -44,4 +44,61 @@
         </div>
     </div>
 </div>
+@endsection --}}
+@extends('layouts.template')
+@section('title')
+Foodseason - Recuperar contraseña
+@endsection
+
+@section("body")
+    @include('partials.header')
+  <body class ="signin-signup-body">
+
+    <section class="register" id="login">
+          <div class="container-signin-signup">
+           <div class="form-signin-signup">
+
+             <h3 class="form-signup-heading">¡Recuperá tu contraseña!</h3>
+<br>
+<br>
+             <form method="POST" action="{{ route('password.update') }}">
+                 @csrf
+
+
+
+
+
+
+           <div class="form-group">
+             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
+
+               @error('email')
+                 <span class="invalid-feedback" role="alert">
+                   <strong>{{ 'Email incorrecto' }}</strong>
+                 </span>
+               @enderror
+           </div>
+
+
+
+
+           <div class="form-group">
+             <button type="submit" class="btn btn-warning btn-signin-signup" style="width:100%">
+                 {{ __('Recuperar') }}
+             </button>
+             <br/><br>
+
+           </div>
+    		  </form>
+
+           </div>
+          </div>
+    </section>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+</body>
 @endsection
